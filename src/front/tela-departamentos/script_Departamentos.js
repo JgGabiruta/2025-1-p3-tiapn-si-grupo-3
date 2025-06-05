66a56d1fb7b17dc76711a7bcb58f1c699393dc22
lhenriquecoelho <luizcoelho102000@gmail.com>
2025-06-05 02:00:01 -0300

    Telas Departamento e Configurações

--- src/front/tela-departamentos/script_Departamentos.js
+++ src/front/tela-departamentos/script_Departamentos.js
@@ -5,105 +5,104 @@ menuButton.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
 });
 
-let listaEmprestimos = document.getElementById("listaEmprestimos");
+let listaDeptos = document.getElementById("listaDeptos");
 
 window.onload = () =>{
 
-    imprimeEmprestimos();
+    imprimeDeptos();
 }
 
 const pesquisar = () => {
     
-    let inputNome = document.getElementById("inputNome").value;
-    inputNome = inputNome.toLowerCase();
+    let inputDepartamento = document.getElementById("inputDepartamento").value;
+    inputDepartamento = inputDepartamento.toLowerCase();
 
     let selectPeca = document.getElementById("selectPeca").value;
-    let selectDepartamento = document.getElementById("selectDepartamento").value
+    let selectNome = document.getElementById("selectNome").value
 
-    let emprestimo = document.getElementsByClassName("emprestimo")
-    let nomes = document.getElementsByClassName("nomes");
+    let Depto = document.getElementsByClassName("Depto")
+    let Departamentos = document.getElementsByClassName("Departamentos");
     let ferramentaCod = document.getElementsByClassName("ferramentaCod");
-    let departamentos = document.getElementsByClassName("departamentos");
+    let Nomes = document.getElementsByClassName("Nomes");
 
-    for (let i = 0; i < nomes.length; i++) {
+    for (let i = 0; i < Departamentos.length; i++) {
         
 
-        if (nomes[i].innerHTML.toLowerCase().includes(inputNome) && selectPeca == "vazio" && selectDepartamento == "vazio") {
+        if (Departamentos[i].innerHTML.toLowerCase().includes(inputDepartamento) && selectPeca == "vazio" && selectNome == "vazio") {
             
-            emprestimo[i].style.display = "flex"
+            Depto[i].style.display = "flex"
 
-        }else if(nomes[i].innerHTML.toLowerCase().includes(inputNome) && ferramentaCod[i].innerHTML.toLowerCase().includes(selectPeca) && selectDepartamento == "vazio"){
+        }else if(Departamentos[i].innerHTML.toLowerCase().includes(inputDepartamento) && ferramentaCod[i].innerHTML.toLowerCase().includes(selectPeca) && selectNome == "vazio"){
             
-            emprestimo[i].style.display = "flex"
+            Depto[i].style.display = "flex"
 
-        }else if(nomes[i].innerHTML.toLowerCase().includes(inputNome) && departamentos[i].innerHTML.includes(selectDepartamento) && selectPeca == "vazio"){
+        }else if(Departamentos[i].innerHTML.toLowerCase().includes(inputDepartamento) && Nomes[i].innerHTML.includes(selectNome) && selectPeca == "vazio"){
             
-            emprestimo[i].style.display = "flex"
+            Depto[i].style.display = "flex"
 
-        }else if(nomes[i].innerHTML.toLowerCase().includes(inputNome) && ferramentaCod[i].innerHTML.toLowerCase().includes(selectPeca) && departamentos[i].innerHTML.includes(selectDepartamento)){
+        }else if(Departamentos[i].innerHTML.toLowerCase().includes(inputDepartamento) && ferramentaCod[i].innerHTML.toLowerCase().includes(selectPeca) && Nomes[i].innerHTML.includes(selectNome)){
 
-            emprestimo[i].style.display = "flex"
+            Depto[i].style.display = "flex"
 
         }else{
 
-            emprestimo[i].style.display = "none";
+            Depto[i].style.display = "none";
         }
     }
 
 }
 
-function imprimeEmprestimos() {
+function imprimeDeptos() {
     
     let str = '';
-    let nomes = ["Getúlio vargas", "Ana Maria", "Veronica Almeida", "Theordor Vieira", "Arthur Gomes"]
-    let departamentos = ["Construção", "Administração", "Operação", "Recursos Humanos", "Construção"]
-    let codigos = ["1287-01", "3242-73", "8923-21", "8392-32", "1282-21"]
+    let Departamentos = ["Construção", "Administração", "Operação", "Recursos Humanos"]
+    let Nomes = ["Getúlio vargas", "Ana Maria", "Veronica Almeida", "Theordor Vieira"]
+    let codigos = ["1287-01", "3242-73", "8923-21", "8392-32",]
 
-    for(let i = 0 ; i < 5 ; i++){
+    for(let i = 0 ; i < 4 ; i++){
 
-        str += `<div class="emprestimo">
+        str += `<div class="Depto">
 
               <div id="grupo1">
 
                 <div>
-                  <p class="label">Nome</p>
-                  <p class="text nomes">${nomes[i]}</p>
+                  <p class="label">Departamento</p>
+                  <p class="text Departamentos">${Departamentos[i]}</p>
                 </div>
 
                 <div>
-                  <p class="label">Data de Empréstimo</p>
-                  <p class="text">02/12/24 - 12:39</p>
-                </div>
+                  <p class="label">Código</p>
+                  <p class="text ferramentaCod">${codigos[i]}</p>
+                </div> 
+
+                
 
               </div>
               
               <div id="grupo2">
 
                 <div>
-                  <p class="label">Departamento</p> 
-                  <p class="text departamentos">${departamentos[i]}</p>
+                  <p class="label">Responsável</p> 
+                  <p class="text Nomes">${Nomes[i]}</p>
                 </div>
 
-                <div>
-                  <p class="label">Ferramenta</p>
-                  <p class="text ferramentaCod">${codigos[i]}</p>
-                </div>              
+                             
               </div>
 
               <div id="grupo3">
 
                 <div>
                   
-                  <p class="label">Data de Devolução <i class="material-icons icon_circle">brightness_1</i></p>
+                  <p class="label">Data de Criação <i class="material-icons icon_circle">brightness_1</i></p>
                   
                   <p class="text">02/12/24</p>
                 </div>
 
-                <button type="submit">Devolver</button>
+                <button type="submit">Detalhes</button>
 
               </div>
             </div>`
     }
 
-    listaEmprestimos.innerHTML = str;
+    listaDeptos.innerHTML = str;
 }
