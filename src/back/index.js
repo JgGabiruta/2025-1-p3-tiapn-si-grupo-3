3a272b20fd4f56ca4e34232afa9fcdaf11e3aad6
João Gabriel Soares da Silva Franco <161077657+JgGabiruta@users.noreply.github.com>
2025-06-06 00:44:42 -0300

    Correção no index.js

--- src/back/index.js
+++ src/back/index.js
@@ -5,11 +5,11 @@ const routes = require('./routes')
 const app = express();
 const path = require('path');
 
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'front')));
 
 app.use(routes);
-app.use(cors());
-app.use(express.json());
 
 
 // Iniciar o servidor
